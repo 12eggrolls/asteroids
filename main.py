8a2731ea78ac4268629e807ff3f85db960ecb733
richtrn5 <richtrn5@aol.com>
2025-05-28 11:46:49 -0400

    60 fps clock tick (added clock object)

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ def main():
     print('Screen width:', SCREEN_WIDTH)
     print('Screen height:', SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
-    
+        dt = clock.tick(60)/1000
 if __name__ == "__main__":
     main()
